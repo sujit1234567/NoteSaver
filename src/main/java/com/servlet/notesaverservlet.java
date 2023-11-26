1c7e46fb41ba23356dddb6a2c43068cfe6abb86a
sujit1234567 <sujity641@gmail.com>
2023-11-26 12:34:10 +0530

    gut

--- src/main/java/com/servlet/notesaverservlet.java
+++ src/main/java/com/servlet/notesaverservlet.java
@@ -21,9 +21,7 @@ import com.helper.FactoryProvider;
 public class notesaverservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public notesaverservlet() {
         super();
         
